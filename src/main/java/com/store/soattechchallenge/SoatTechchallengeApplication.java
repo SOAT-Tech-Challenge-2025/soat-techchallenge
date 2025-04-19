98b995493c3ed4fd589874cef4fb6d59c9427bf3
KarenL19 <karen-_-19@outlook.com>
2025-04-19 18:44:55 -0300

    Initializing project

---
+++ src/main/java/com/store/soattechchallenge/SoatTechchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.store.soattechchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoatTechchallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoatTechchallengeApplication.class, args);
+    }
+
+}
