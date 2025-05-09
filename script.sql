82f493dc21d92ec3dfa1603bda682d11935a9301
KarenL19 <karen-_-19@outlook.com>
2025-05-08 21:39:27 -0300

    implements docker and database

--- script.sql
+++ script.sql
@@ -18,7 +18,7 @@ CREATE TABLE tb_categoria_itens (
 
 -- Tabela de Produtos
 CREATE TABLE tb_produto (
-    id SERIAL PRIMARY KEY,
+    id VARCHAR(255) PRIMARY KEY,
     nm_produto VARCHAR(100),
     id_categoria INTEGER REFERENCES tb_categoria_itens(id),
     vl_unitario_produto NUMERIC(10, 2),
@@ -29,7 +29,7 @@ CREATE TABLE tb_produto (
 
 -- Tabela de Carrinho de Pedido
 CREATE TABLE tb_carrinho_pedido (
-    id SERIAL PRIMARY KEY,
+    id VARCHAR(255) PRIMARY KEY,
     vl_total_pedido NUMERIC(10, 2),
     minutagem INTEGER, -- pode ser o tempo total estimado
     id_cliente INTEGER REFERENCES tb_cliente(id),
@@ -38,7 +38,7 @@ CREATE TABLE tb_carrinho_pedido (
 
 -- Tabela de relação entre Carrinho e Produto
 CREATE TABLE tb_carrinho_produto (
-    id SERIAL PRIMARY KEY,
+    id VARCHAR(255) PRIMARY KEY,
     id_produto INTEGER REFERENCES tb_produto(id),
     qt_item INTEGER,
     vl_qt_item NUMERIC(10, 2),
@@ -47,7 +47,7 @@ CREATE TABLE tb_carrinho_produto (
 
 -- Tabela de Pagamentos
 CREATE TABLE tb_pagamento (
-   id SERIAL PRIMARY KEY,
+   id VARCHAR(255) PRIMARY KEY,
    id_externo VARCHAR(100) UNIQUE,
    st_pagamento VARCHAR(50),
    vl_total_pedido NUMERIC(10, 2),
@@ -59,7 +59,7 @@ CREATE TABLE tb_pagamento (
 
 -- Tabela de Preparação
 CREATE TABLE tb_preparacao (
-    id SERIAL PRIMARY KEY,
+    id VARCHAR(255) PRIMARY KEY,
     posicao_preparacao INTEGER,
     estimativa_de_pronto TIMESTAMP,
     tempo_de_preparacao INTEGER,
