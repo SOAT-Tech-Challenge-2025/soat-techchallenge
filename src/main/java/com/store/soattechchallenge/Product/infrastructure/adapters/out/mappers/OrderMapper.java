8381656dec3dac028ba46750f587a5abfc550d9e
KarenL19 <karen-_-19@outlook.com>
2025-05-20 20:45:46 -0300

    Implements Order

--- src/main/java/com/store/soattechchallenge/Product/infrastructure/adapters/out/mappers/OrderMapper.java
+++
@@ -1,6 +0,0 @@
-package com.store.soattechchallenge.Product.infrastructure.adapters.out.mappers;
-
-public interface OrderMapper {
-
-
-}
--- src/main/java/com/store/soattechchallenge/Product/infrastructure/adapters/out/repository/OrderAdaptersGetRepository.java
+++
@@ -1,9 +0,0 @@
-package com.store.soattechchallenge.Product.infrastructure.adapters.out.repository;
-
-import com.store.soattechchallenge.Product.infrastructure.adapters.out.entity.OrderEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderAdaptersGetRepository extends JpaRepository<OrderEntity,Long> {
-}
