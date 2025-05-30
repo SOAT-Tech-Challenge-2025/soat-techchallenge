a19119fc988fec45fab24ebff2a191e8d83d0e34
KarenL19 <karen-_-19@outlook.com>
2025-05-30 19:31:18 -0300

    Implements main

--- script.sql
+++ script.sql
@@ -1,77 +1,77 @@
 -- Tabela de Clientes
 CREATE TABLE tb_cliente (
-    id UUID PRIMARY KEY,
-    nm_cliente VARCHAR(100),
-    ds_email VARCHAR(100),
-    nr_documento VARCHAR(20),
-    dt_inclusao TIMESTAMP,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                            id UUID PRIMARY KEY,
+                            nm_cliente VARCHAR(100),
+                            ds_email VARCHAR(100),
+                            nr_documento VARCHAR(20),
+                            dt_inclusao TIMESTAMP,
+                            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Tabela de Categorias
 CREATE TABLE tb_categoria_itens (
-    id SERIAL PRIMARY KEY,
-    nm_categoria VARCHAR(100),
-    dt_inclusao DATE,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                                    id SERIAL PRIMARY KEY,
+                                    nm_categoria VARCHAR(100),
+                                    dt_inclusao DATE,
+                                    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Tabela de Produtos
 CREATE TABLE tb_produto (
-    id SERIAL PRIMARY KEY,
-    nm_produto VARCHAR(100),
-    id_categoria INTEGER REFERENCES tb_categoria_itens(id),
-    vl_unitario_produto NUMERIC(10, 2),
-    tempo_de_preparo_produto INTEGER, -- tempo em minutos
-    dt_inclusao DATE,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                            id SERIAL PRIMARY KEY,
+                            nm_produto VARCHAR(100),
+                            id_categoria INTEGER REFERENCES tb_categoria_itens(id),
+                            vl_unitario_produto NUMERIC(10, 2),
+                            tempo_de_preparo_produto INTEGER, -- tempo em minutos
+                            dt_inclusao DATE,
+                            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Tabela de Carrinho de Pedido
 CREATE TABLE tb_carrinho_pedido (
-    id VARCHAR(255) PRIMARY KEY,
-    vl_total_pedido NUMERIC(10, 2),
-    tempo_de_preparo_pedido INTEGER,
-    id_cliente VARCHAR(255),
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                                    id VARCHAR(255) PRIMARY KEY,
+                                    vl_total_pedido NUMERIC(10, 2),
+                                    tempo_de_preparo_pedido INTEGER,
+                                    id_cliente VARCHAR(255),
+                                    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Tabela de relação entre Carrinho e Produto
 CREATE TABLE tb_carrinho_produto (
-    id VARCHAR(255),
-    id_produto INTEGER,
-    qt_item INTEGER,
-    vl_qt_item NUMERIC(10, 2),
-    PRIMARY KEY (id, id_produto),
-    FOREIGN KEY (id) REFERENCES tb_carrinho_pedido(id)
+                                     id VARCHAR(255),
+                                     id_produto INTEGER,
+                                     qt_item INTEGER,
+                                     vl_qt_item NUMERIC(10, 2),
+                                     PRIMARY KEY (id, id_produto),
+                                     FOREIGN KEY (id) REFERENCES tb_carrinho_pedido(id)
 );
 
 -- Tabela de Pagamentos
 CREATE TABLE tb_pagamento (
-    id VARCHAR(255) PRIMARY KEY,
-    id_externo VARCHAR(100) UNIQUE,
-    st_pagamento VARCHAR(50),
-    vl_total_pedido NUMERIC(10, 2),
-    codigo_qr TEXT,
-    expiracao TIMESTAMP,
-    dt_inclusao TIMESTAMP,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                              id VARCHAR(255) PRIMARY KEY,
+                              id_externo VARCHAR(100) UNIQUE,
+                              st_pagamento VARCHAR(50),
+                              vl_total_pedido NUMERIC(10, 2),
+                              codigo_qr TEXT,
+                              expiracao TIMESTAMP,
+                              dt_inclusao TIMESTAMP,
+                              timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Tabela de Preparação
 CREATE TABLE tb_preparacao (
-    id VARCHAR(255) PRIMARY KEY,
-    posicao_preparacao INTEGER,
-    estimativa_de_pronto TIMESTAMP,
-    tempo_de_preparacao INTEGER,
-    st_preparacao VARCHAR(50),
-    dt_inclusao TIMESTAMP,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                               id VARCHAR(255) PRIMARY KEY,
+                               posicao_preparacao INTEGER,
+                               estimativa_de_pronto TIMESTAMP,
+                               tempo_de_preparacao INTEGER,
+                               st_preparacao VARCHAR(50),
+                               dt_inclusao TIMESTAMP,
+                               timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE tb_gera_pedido (
-    id VARCHAR(4) PRIMARY KEY,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                                id VARCHAR(4) PRIMARY KEY,
+                                timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE OR REPLACE FUNCTION generate_order_id() RETURNS VARCHAR AS $$
