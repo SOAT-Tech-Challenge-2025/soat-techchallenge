513089f9fc10ec3d4fcaaa199acbd8b2b00bb5d7
KarenL19 <karen-_-19@outlook.com>
2025-05-30 19:45:37 -0300

    Implements main

--- script.sql
+++ script.sql
@@ -110,7 +110,7 @@ END;
    $$ LANGUAGE plpgsql;
 
 
--- Insert initial data
+-- Insert initial data categories
 INSERT INTO public.tb_categoria_itens
 (nm_categoria, dt_inclusao, "timestamp")
 VALUES('LANCHES', '2025-05-29', '2025-05-29 21:43:35.015');
@@ -126,3 +126,21 @@ VALUES('SOBREMESAS', '2025-05-29', '2025-05-29 21:44:10.040');
 INSERT INTO public.tb_categoria_itens
 (nm_categoria, dt_inclusao, "timestamp")
 VALUES('MOLHOS E ADICIONAIS', '2025-05-29', '2025-05-29 21:44:24.669');
+
+-- Insert initial data products
+
+INSERT INTO public.tb_produto
+(id, nm_produto, id_categoria, vl_unitario_produto, tempo_de_preparo_produto, dt_inclusao, "timestamp")
+VALUES(1, 'HAMBURGUER', 1, 20.00, 10, '2025-05-30', '2025-05-30 22:40:10.254');
+INSERT INTO public.tb_produto
+(id, nm_produto, id_categoria, vl_unitario_produto, tempo_de_preparo_produto, dt_inclusao, "timestamp")
+VALUES(2, 'BATATA FRITA', 2, 15.00, 3, '2025-05-30', '2025-05-30 22:40:24.049');
+INSERT INTO public.tb_produto
+(id, nm_produto, id_categoria, vl_unitario_produto, tempo_de_preparo_produto, dt_inclusao, "timestamp")
+VALUES(3, 'COCA COLA', 3, 10.00, 0, '2025-05-30', '2025-05-30 22:40:33.170');
+INSERT INTO public.tb_produto
+(id, nm_produto, id_categoria, vl_unitario_produto, tempo_de_preparo_produto, dt_inclusao, "timestamp")
+VALUES(4, 'SORVETE', 4, 9.00, 2, '2025-05-30', '2025-05-30 22:41:38.421');
+INSERT INTO public.tb_produto
+(id, nm_produto, id_categoria, vl_unitario_produto, tempo_de_preparo_produto, dt_inclusao, "timestamp")
+VALUES(5, 'MAIONESE', 5, 3.00, 0, '2025-05-30', '2025-05-30 22:41:49.249');
